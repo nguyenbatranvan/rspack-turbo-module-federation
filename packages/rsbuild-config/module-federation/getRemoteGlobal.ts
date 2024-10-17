ba25c95f7edd2053f654d6b9d10044b5225bf8ac
nguyenbatranvan <nguyenbatranvan96@gmail.com>
2024-10-17 14:40:14 +0700

    update fix error runtime

--- packages/rsbuild-config/module-federation/getRemoteGlobal.ts
+++ packages/rsbuild-config/module-federation/getRemoteGlobal.ts
@@ -1,17 +1,21 @@
-import { Remotes } from "./module-fedeation.type";
+import {Remotes} from "./module-fedeation.type";
 
 declare global {
-	var __ICONDO__REMOTES: any;
+    var __ICONDO__REMOTES: any;
 }
 export const getGlobalRemotes = () => {
-	return window.__ICONDO__REMOTES || [];
+    return window.__ICONDO__REMOTES || [];
 };
 
 export const setGlobalRemotes = (data: Remotes) => {
-	// if (!window.__ICONDO__REMOTES || !window.__ICONDO__REMOTES?.length) {
-	//     window.__ICONDO__REMOTES = data;
-	// }
-	// localStorage.setItem("__ICONDO__REMOTES", JSON.stringify(window.__ICONDO__REMOTES || data || []));
+    if (!window.__ICONDO__REMOTES || !window.__ICONDO__REMOTES?.length) {
+        window.__ICONDO__REMOTES = data;
+    }
+    localStorage.setItem("__ICONDO__REMOTES", JSON.stringify(window.__ICONDO__REMOTES || data || []));
+    // if (!window.__ICONDO__REMOTES || !window.__ICONDO__REMOTES?.length) {
+    //     window.__ICONDO__REMOTES = data;
+    // }
+    // localStorage.setItem("__ICONDO__REMOTES", JSON.stringify(window.__ICONDO__REMOTES || data || []));
 };
 // const BroadcastLogout = new BroadcastChannel("auth-channel");
 // BroadcastLogout.onmessage = () => {
